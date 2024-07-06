efe9c4a5eef3c14bc319f6c61f3e78667743fef6
Bekmuhammad-Mamadiyev <bekmuhammadmamadiyev90@gmail.com>
2024-07-06 14:08:31 +0500

    instagram clone project

---
+++ users/views.py
@@ -0,0 +1,11 @@
+from rest_framework import permissions
+from rest_framework.generics import CreateAPIView
+from rest_framework.decorators import permission_classes
+from .serializers import SignUpSerializer
+from .models import User
+
+class CreateUserView(CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = SignUpSerializer
+
